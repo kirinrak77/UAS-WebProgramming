9201f92ded1a9da03e34422a678eddd2d31fc916
alwirihad <122687990+alwirihad@users.noreply.github.com>
2023-06-26 14:06:22 +0700

    Add files via upload
    File koneksi database

---
+++ konek.php
@@ -0,0 +1,14 @@
+<?php
+$db_host    = "localhost";
+$db_user    = "root";
+$db_pass    = "";
+$db_name    = "multiuser";
+
+$koneksi    = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if($koneksi){  
+    echo("Koneksi Berhasil");
+}
+else{
+    echo("Koneksi Gagal");
+}
+?>
